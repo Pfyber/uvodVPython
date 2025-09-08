821590d08696e01e6b5c9d8afb3a78b521967f22
Pfyber <luka.colaric@gmail.com>
2025-09-08 06:48:27 +0000

    uvod v spremenljivke

---
+++ main.py
@@ -0,0 +1,28 @@
+# komentar
+# spremenljivke
+
+# int (Integer) - celo število
+# spremenljivka - ime = vrednost
+# Starost Človeka
+#StarostČloveka = 15
+#camel case
+starostCloveka = 30
+#snake case
+starost_cloveka = 30
+
+# string - niz znakov
+# "" = shift + 2
+ime = "Luka7887@@@"
+
+# float - decimalna števila
+pi = 3.14
+
+# boolean - logične vrednosti
+# True ali False
+
+pridenDijak = True
+
+
+# print() - funkcija
+print("Brina")
+print(starost_cloveka)
